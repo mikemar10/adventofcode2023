f98973745d4e9ec6d77a065dd8948d35525505fe
Mike Martin <mike@unsupported.me>
2023-12-17 12:02:54 -0600

    Reworks day02_first to use filter_map and sum

--- day02/src/first.rs
+++ day02/src/first.rs
@@ -1,11 +1,16 @@
 use day02::*;
 fn main() {
     let input = include_str!("../input.txt");
-    let sum = input
+    let sum: usize = input
         .split('\n')
         .take_while(|s| !s.is_empty())
-        .map(Game::parse_game)
-        .filter(|game| game.is_game_possible(12, 13, 14))
-        .fold(0, |acc, game| acc + game.id);
+        .filter_map(|g| {
+            let game = Game::parse_game(g);
+            match game.is_game_possible(12, 13, 14) {
+                true => Some(game.id),
+                false => None,
+            }
+        })
+        .sum();
     println!("Answer: {}", sum);
 }
